d630e3744a78fdffa111db1ca8d97c42677e8c2c
Samipya ghimire <samipya1117@gmail.com>
2026-03-12 11:15:16 +0545

    Added monitoring

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import DashboardView from '@/views/DashboardView.vue';
 import IncidentDetailView from '@/views/IncidentDetailView.vue';
+import MonitoringView from '@/views/MonitoringView.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'dashboard', component: DashboardView },
     { path: '/incidents/:id', name: 'incident-detail', component: IncidentDetailView },
+    { path: '/monitoring', name: 'monitoring', component: MonitoringView },
   ],
 });
 
